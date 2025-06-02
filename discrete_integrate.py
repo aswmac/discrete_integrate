e41d607748c893d20462143df00399fdedf11abe
aswmac <aswmac@yahoo.com>
2025-06-02 15:00:10 -0500

    Update discrete_integrate.py

--- discrete_integrate.py
+++ discrete_integrate.py
@@ -4,14 +4,10 @@ def discreteIntegrate(coeff_list):
 	dim = len(coeff)
 	for i in range(dim - 1, -1, -1):
 		hgterm = coeff[i] # binoimial term factors count down/up on numerator, and on denominator
-		#print("hgterm =", hgterm, end=" ")
 		coeff[i] = coeff[i]//(i + 1)
 		for j in range(i - 1, -1, -1):
-			#print("coeff[", j, "] = ", coeff[j], end = " ")
 			hgterm = hgterm*(j + 1)//(1 + i - j) # starts at 2 for j = i - 1, counts down by j, 2 = k - j for j = i - 1, k = 2 + i - 1
 			coeff[j] = coeff[j] - hgterm
-			#print(", hgterm =", hgterm, " new coeff[", j, "]=", coeff[j], end=" -- ")
-		#print(" ")
 	return [0] + coeff # the first term is the constant term thus is anything really, here return 0 for that
 
 def polyFromShift(coeff_list, s):
